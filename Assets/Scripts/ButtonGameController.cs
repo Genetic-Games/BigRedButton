3411a8c7023efbdf7a6f7a7ab1b95cea34b706f3
Ian Santagata <7070901+iansantagata@users.noreply.github.com>
2018-08-08 23:21:08 -0400

    Got the main flow of the game working, splash to main menu to button game to high score visibility

---
+++ Assets/Scripts/ButtonGameController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonGameController : MonoBehaviour {
+
+    private uint highScore = 0;
+    private string highScoreText = "High Score: ";
+
+    /// <summary>
+    /// Requires a text box to display the high score
+    /// </summary>
+    public Text highScoreTextBox;
+
+	// Use this for initialization
+	void Start ()
+    {
+        highScore = 0;
+
+        if (Debug.isDebugBuild)
+        {
+            Debug.Assert(highScoreTextBox != null);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        highScoreTextBox.text = highScoreText + highScore;
+	}
+
+    /// <summary>
+    /// Call this every time the button is pressed
+    /// </summary>
+    public void IncrementCount()
+    {
+        highScore++;
+    }
+}
