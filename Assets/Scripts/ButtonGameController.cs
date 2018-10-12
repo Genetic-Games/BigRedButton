36ccdc16df96d6af29f20cb64e355f13da76075f
Ian Santagata <7070901+iansantagata@users.noreply.github.com>
2018-10-11 22:52:32 -0400

    Added high score and current score functionality to main game screen

--- Assets/Scripts/ButtonGameController.cs
+++ Assets/Scripts/ButtonGameController.cs
@@ -3,36 +3,106 @@ using UnityEngine.UI;
 
 public class ButtonGameController : MonoBehaviour {
 
-    private uint highScore = 0;
+    private bool isCurrentScoreHighScore = false;
+
+    private ulong currentScore = 0;
+    private ulong highScore = 0;
+
+    private string currentScoreText = "Current Score: ";
     private string highScoreText = "High Score: ";
+    private string newHighScoreText = "New High Score!";
+
+    private string username;
+
+    /// <summary>
+    /// Requires a text box to display the current score
+    /// </summary>
+    public Text currentScoreTextBox;
 
     /// <summary>
     /// Requires a text box to display the high score
     /// </summary>
     public Text highScoreTextBox;
 
-	// Use this for initialization
+    /// <summary>
+    /// Requires a text box to display text that a new high score has been reached
+    /// </summary>
+    public Text newHighScoreTextBox;
+
+	/// <summary>
+    /// Initialize the game state
+    /// </summary>
 	void Start ()
     {
-        highScore = 0;
+        UpdateTextBox(currentScoreTextBox, currentScoreText + currentScore);
+        UpdateTextBox(highScoreTextBox, highScoreText + highScore);
+        UpdateTextBox(newHighScoreTextBox, newHighScoreText);
+        newHighScoreTextBox.gameObject.SetActive(isCurrentScoreHighScore);
 
         if (Debug.isDebugBuild)
         {
-            Debug.Assert(highScoreTextBox != null);
+            Debug.Assert(currentScoreTextBox != null, "Error: Current Score Text Box Unspecified / Null");
+            Debug.Assert(highScoreTextBox != null, "Error: High Score Text Box Unspecified / Null");
+            Debug.Assert(newHighScoreTextBox != null, "Error: New High Score Text Box Unspecified / Null");
         }
 	}
 	
-	// Update is called once per frame
+	/// <summary>
+    /// Update the game state every frame
+    /// </summary>
 	void Update ()
     {
-        highScoreTextBox.text = highScoreText + highScore;
 	}
+
+    /// <summary>
+    /// Use to check if the current score is a new high score and set high score and flags if so
+    /// </summary>
+    /// <param name="score"/>
+    private void SetHighScore(ulong score)
+    {
+        if (!isCurrentScoreHighScore)
+        {
+            isCurrentScoreHighScore = true;
+            newHighScoreTextBox.gameObject.SetActive(isCurrentScoreHighScore);
+        }
+
+        highScore = score;
+    }
+
+    /// <summary>
+    /// Helper function to determine if the current score is the new highest
+    /// </summary>
+    /// <param name="current"></param>
+    /// <param name="highest"></param>
+    /// <returns></returns>
+    private bool IsNewHighScore(ulong current, ulong highest)
+    {
+        return current >= highest;
+    }
+
+    /// <summary>
+    /// Helper function to update a text box with a particular string
+    /// </summary>
+    /// <param name="box"></param>
+    /// <param name="newText"></param>
+    private void UpdateTextBox(Text box, string newText)
+    {
+        box.text = newText;
+    }
 
     /// <summary>
     /// Call this every time the button is pressed
     /// </summary>
     public void IncrementCount()
     {
-        highScore++;
+        currentScore++;
+        UpdateTextBox(currentScoreTextBox, currentScoreText + currentScore);
+
+        // Check if it's a new high score and handle if it is
+        if (IsNewHighScore(currentScore, highScore))
+        {
+            SetHighScore(currentScore);
+            UpdateTextBox(highScoreTextBox, highScoreText + highScore);
+        }
     }
 }
